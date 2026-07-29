e8195ef9c4b0d40e66ef0769e1ac72014c71d7da
leo <leandrofuerte75@gmail.com>
2026-07-29 10:45:15 -0700

    temperature's average

---
+++ 9.2:non-empty-lists/list-of-temperatures-average.rkt
@@ -0,0 +1,41 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname list-of-temperatures-average) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;a List-Of-Temperatures is one of:
+;-- '()
+;-- (cons CTemperature List-Of-Temperatures)
+;interpretation: an arbitrary large list of temperatures
+
+;a CTemperature is a Number greater than -272
+
+;List-Of-Temperatures->Number
+;computes the average temperature
+(check-expect (average (cons 1 (cons 2 (cons 3 '())))) 2)
+(define (average l) 
+  (/ (sum l) (how-many l))
+)
+
+;List-Of-Temperatures->Number
+;computes the sum of every temperature on the list
+(check-expect (sum '()) 0)
+(check-expect (sum (cons 1 (cons 2 (cons 3 '())))) 6)
+(check-expect (sum (cons 9 (cons 2 (cons 3 '())))) 14)
+(define (sum l)
+  (cond
+    [(empty? l) 0]
+    [else (+(first l)(sum (rest l)))]
+  )
+)
+
+;List-Of-Temperatures->Number
+;computes how many temperatures the list have
+(check-expect (sum '()) 0)
+(check-expect (how-many (cons 1 (cons 2 '()))) 2)
+(check-expect (how-many (cons 1 (cons 2 '()))) 2)
+(define (how-many l)
+  (cond
+    [(empty? l) 0]
+    [else (add1 (how-many (rest l)))]
+  )
+)
