4b20575578e20fb09cc5e2e001e2c263f1a9c59a
leo <leandrofuerte75@gmail.com>
2026-08-02 13:46:06 -0700

    data definitions, examples (ex. 199) and itunes.xml

---
+++ 12.2:real-world-data-itunes/itunes.rkt
@@ -0,0 +1,31 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname itunes) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(require 2htdp/batch-io)
+(require 2htdp/itunes)
+
+(define IL "itunes.xml")
+
+;LTracks
+(define itunes-tracks
+  (read-itunes-as-tracks IL))
+
+;a Date is a structure
+;  (make-date N N N N N N)
+;interpretations: year month day hour minute second
+;example: (make-date 2026 8 2 13 44 40)
+
+;a Track is a structure
+;  (make-track String String String  N N Date N Date)
+;interpretation: title,artist, album, playing time, position in album,
+;date it was added, played times, date when kt was last played
+;example: (make-track 
+;  "Wild Child" 
+;  "Enya" 
+;  "A Day Without Rain" 
+;  227996 
+;  2 
+;  (make-date 2002 7 17 3 55 14) 
+;  20 
+;  (make-date 2011 5 17 17 35 13))
