cf91df3c14329672b135e8298bf4fac148c9e622
leo <leandrofuerte75@gmail.com>
2026-08-07 15:46:14 -0700

    siu html generator

---
+++ INTERMEZZO-II/html-generator.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname html-generator) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(require 2htdp/web-io)
+
+;String String -> ... deeply nested list ...
+;produces a web page with given author and title
+(define (web-page author title)
+  `(html
+     (head
+       (title ,title)
+       (meta ((http-equiv "content-type")
+              (content "text-html"))))
+     (body
+       (h1 ,title)
+       (p "I, " ,author ", made this page.")))) 
+
+(define my-page (web-page "Leo" "Yes, I did it"))
+
+(show-in-browser my-page)
