2671b0ff52ea41a6e0806a5b3c114c3832849b1c
leo <leandrofuerte75@gmail.com>
2026-08-07 10:50:38 -0700

    big-bang expression

--- 12.8:finite-state-machines/fsm.rkt
+++ 12.8:finite-state-machines/fsm.rkt
@@ -2,6 +2,9 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname fsm) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
+(require 2htdp/universe)
+(require 2htdp/image)
+
 ;a FSM is one of:
 ;-- '()
 ;-- (cons Transition FSM)
@@ -21,10 +24,34 @@
   (make-transition "yellow" "red")
   (make-transition "green" "yellow")))
 
+(define fsm-bw (list 
+  (make-transition "white" "black")
+  (make-transition "black" "white")))
 
 ;FMS-State FMS-State->Boolean
+;checks the equality of states
 (check-expect (state=? "red" "yellow") #false)
 (check-expect (state=? "yellow" "yellow") #true)
 (define (state=? s1 s2)
   (string=? s1 s2)
 )
+
+;a SimulationState.v1 is an FSM-State
+(define initial-state "white")
+
+;FSM->???
+;math the keys pressed with the given FSM
+(define (simulate an-fsm)
+  (big-bang initial-state
+    [to-draw render-state.v1]
+    [on-key find-next-state.v1]
+  )
+)
+
+;SimulationState.v1->Image
+;renders the state
+(define (render-state.v1 s) empty-image)
+
+;SimulationState.v1 KeyEvent->SimulationState.v1
+;changes to the next
+(define (find-next-state.v1 s ke) s)
