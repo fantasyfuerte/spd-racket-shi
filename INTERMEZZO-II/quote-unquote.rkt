d3d1d17f8470d0e3e7ca8d1977922b17ff2bca11
leo <leandrofuerte75@gmail.com>
2026-08-07 15:16:30 -0700

    ?

---
+++ INTERMEZZO-II/quote-unquote.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname quote-unquote) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(define qt (quote (1 2 3)))
+(define qt1 '(1 2 3))
+
+(equal? qt qt1) ;#true
