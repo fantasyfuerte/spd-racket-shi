3c31762a304a9d4d0f202b003b7badb7ee4f6429
leo <leandrofuerte75@gmail.com>
2026-08-08 11:33:15 -0700

    exercise 252

--- 15.1:abstractions-from-examples/tabulate.rkt
+++ 15.1:abstractions-from-examples/tabulate.rkt
@@ -2,6 +2,8 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname tabulate) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
+(require 2htdp/image)
+
 ;Number->[List-of Number]
 ;tabulates sin between n and 0 (incl.) in a list 
 (define (tab-sin n)
@@ -39,5 +41,47 @@
         (fold1 f (sub1 n))
       )
     ]
+  )
+)
+
+;[List-of Number] -> Number
+(define (product l)
+  (cond
+    [(empty? l) 1]
+    [else (* (first l) (product (rest l)))]
+  )
+)
+
+;[List-of Posn]->Image
+(define (image* l)
+  (cond
+    [(empty? l) emt]
+    [else (place-dot (first l) (image* (rest l)))]
+  )
+)
+
+;Posn Image -> Image
+(define (place-dot p img)
+  (place-image
+    dot
+    (posn-x p) (posn-y p)
+    img))
+
+;graphical constants
+(define emt
+  (empty-scene 100 100))
+(define dot
+  (circle 3 "solid" "red"))
+
+(check-expect 
+  (fold2 `( ,(make-posn 3 4) ,(make-posn 1 9)) place-dot emt)
+  (image* `( ,(make-posn 3 4) ,(make-posn 1 9))))  
+(check-expect
+  (fold2 '(1 2 4 52 5) * 1)
+  (product '(1 2 4 52 5)))
+(define (fold2 l f d)
+  (cond
+    [(empty? l) d]
+    [else (f (first l) (fold2 (rest l) f d))]
   )
 )
