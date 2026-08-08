a4e04cb437147afd71129b541cd82ee5e8c9eb03
leo <leandrofuerte75@gmail.com>
2026-08-07 21:00:48 -0700

    contains?

---
+++ 14.1:similarities-in-functions/contains.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname contains) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;List-Of-Strings String->Boolean
+;yields true if s is in los
+(define (contains? los s)
+  (cond
+    [(empty? los) #false]
+    [else (or (string=? s (first los))
+              (contains? s (rest los)))
+    ]
+  )
+)
