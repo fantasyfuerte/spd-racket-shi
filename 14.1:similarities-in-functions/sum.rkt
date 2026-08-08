2dbb078f909af9c255346590e057479cf40d87fc
leo <leandrofuerte75@gmail.com>
2026-08-07 21:09:06 -0700

    exercise 236

---
+++ 14.1:similarities-in-functions/sum.rkt
@@ -0,0 +1,31 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname sum) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;Lon->Lon
+;adds 1 to each item on l
+(check-expect (add1* '(1 2 3)) '(2 3 4))
+(define (add1* l)
+  (cond
+    [(empty? l) '()]
+    [else 
+      (cons
+      (add1 (first l))
+      (add1* (rest l)))
+    ]
+  )
+)
+
+;Lon->Lon
+;adds 5 to each item on l
+(check-expect (plus5 '(1 2 3)) '(6 7 8))
+(define (plus5 l)
+  (cond
+    [(empty? l) '()]
+    [else
+      (cons
+        (+ (first l) 5)
+        (plus5 (rest l)))
+    ]
+  )
+)
