29e739bcfc5311bdbdf5aaf2eb86089a664e2203
leo <leandrofuerte75@gmail.com>
2026-08-09 12:34:57 -0700

    identity matrix generator

---
+++ 16.3:local-definitions-add-expressive-power/identityM.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname identityM) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;Number -> [List-of [List-of Number]]
+;creates an identity matrix
+(define (identityM l)
+  (local (
+  (define (f n)
+    (local(
+      (define (arrange1 v)
+        (if (= n v) 1 0)
+      )
+    )(build-list l arrange1))
+  )
+  )(build-list l f))
+)
+
+(identityM 3)
+(identityM 5)
