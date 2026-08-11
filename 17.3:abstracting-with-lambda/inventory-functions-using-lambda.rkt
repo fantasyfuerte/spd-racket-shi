ab245369b2255bde216570f34d17605318f1a5c6
leo <leandrofuerte75@gmail.com>
2026-08-11 10:14:35 -0700

    exercise 286

---
+++ 17.3:abstracting-with-lambda/inventory-functions-using-lambda.rkt
@@ -0,0 +1,69 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname inventory-functions-using-lambda) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(define-struct inv-i [name sales-price acq-price])
+;[List-of InventoryItem] -> [List-of InventoryItem]
+;sorts l by the difference between sales and acquisition price
+(define (sort-by-profit l)
+  (sort l (lambda (i1 i2)(> (- (inv-i-sales-price i1) (inv-i-acq-price i1))
+         (- (inv-i-sales-price i2) (inv-i-acq-price i2)))))
+)
+
+;Number [List-of InventoryItem] -> [List-of InventoryItem]
+;produces a list of all those items whose sales price is below ua
+(define (eliminate-expensive ua l)
+  (local(
+    ;InventoryItem -> Boolean
+    ;yields true if the sales price is below ua
+    (define (below-ua? i) (> (inv-i-sales-price i) ua))    
+  )
+  (filter below-ua? l))
+)
+
+;String [List-of InventoryItem] -> [List-of InventoryItem]
+;produces a list of all those items whose names isn't ty
+(define (recall ty l)
+  (local(
+    ;InventoryItem -> Boolean
+    ;yields true if the isn't ty
+    (define (not-ty? i) (not (string=? (inv-i-name i) ty)))    
+  )
+  (filter not-ty? l))
+)
+
+;[List-of String] [List-of String] -> [List-of String]
+;produces the names who are in both lists
+(define (selection l1 l2)
+  (local(
+    ;String -> Boolean
+    ;yields true if i is in l2
+    (define (is-in-l2? i) 
+      (member? i l2)
+    )
+  )
+  (filter is-in-l2? l1))
+)
+
+(define (zero-to-nsub1 n)
+  (local(
+    (define (f x) x)
+  )
+  (build-list n f))
+)
+
+(define (one-to-one/n n)
+  (local(
+    (define (f x) (/ 1 (add1 x)))
+  )
+  (build-list n f))
+)
+
+(define (first-n-odds n)
+  (local(  
+    (define (f x)
+      (add1(* 2 x))  
+    )
+  )
+  (build-list n f))
+)
