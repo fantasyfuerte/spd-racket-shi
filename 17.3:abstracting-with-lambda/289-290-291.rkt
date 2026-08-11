c8f2a7c1fdacf4232ac3ea769c487f25b210a64a
leo <leandrofuerte75@gmail.com>
2026-08-11 10:31:08 -0700

    exercise 289

---
+++ 17.3:abstracting-with-lambda/289-290-291.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 289-290-291) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;exercise 289
+(check-expect (find-name "one" '("onee" "two")) #true)
+(check-expect (find-name "0one" '("onee" "two")) #false)
+(define (find-name n l)
+  (ormap (lambda (x) (string-contains? n x)) l) 
+)
+
+(check-expect (start-with-a? '("a" "ab")) #true)
+(check-expect (start-with-a? '("r" "ab")) #false)
+(define (start-with-a? l)
+  (andmap (lambda (x) (string=? "a" (first (explode x)))) l) 
+)
