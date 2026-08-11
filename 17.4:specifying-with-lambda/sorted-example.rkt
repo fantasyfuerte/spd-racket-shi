060dfab0e4200937f9ccafb063e8b0e85758830a
leo <leandrofuerte75@gmail.com>
2026-08-11 12:20:19 -0700

    the sort testing problem

---
+++ 17.4:specifying-with-lambda/sorted-example.rkt
@@ -0,0 +1,42 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname sorted-example) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;[X] [List-of X] [X X -> Boolean] -> [List-of X]
+;sorts l according to cmp
+(check-expect (sort-cmp '("c" "b") string<?) '("b" "c"))
+(check-expect (sort-cmp '(2 1 3 4 6 5) <) '(1 2 3 4 5 6))
+(check-satisfied (sort-cmp '("c" "b") string<?)
+                 (sorted string<?))
+(check-satisfied (sort-cmp '(2 1 3 4 6 5) <)
+                 (sorted <))
+(define (sort-cmp l cmp)
+  (local (
+    ;[List-of X] -> [List-of X]
+    ;produces a variant of l sorted by cmp
+    (define (isort l)
+      (cond
+        [(empty? l) '()]
+        [else (insert (first l) (isort (rest l)))]
+      )
+    )
+    
+    ;X [List-of X] -> [List-of X]
+    ;inserts n into the sorted list of numbers
+    (define (insert n l)
+      (cond
+        [(empty? l) (cons n '())]
+        [else (if (cmp n (first l))
+                  (cons n l)
+                  (cons (first l) (insert n (rest l))))])))
+  (isort l)))
+
+;[X X -> Boolean] -> [ [List-of X] -> Boolean]
+;produces a function that determines wheter some list is sorted
+;according to cmp
+(check-expect [(sorted string<?) '("b" "c")] #true)
+(check-expect [(sorted <) '(1 2 3 4 5)] #true)
+(define (sorted cmp)
+  (lambda (l)
+    (local((define (sorted/l l) ...))
+      ...)))
