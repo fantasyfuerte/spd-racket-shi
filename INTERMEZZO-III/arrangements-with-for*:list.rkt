2fdf7bb01d8474820353e456a06825609b1769a8
leo <leandrofuerte75@gmail.com>
2026-08-14 08:32:44 -0700

    or-map

--- INTERMEZZO-III/arrangements-with-for*:list.rkt
+++ INTERMEZZO-III/arrangements-with-for*:list.rkt
@@ -27,3 +27,9 @@
 (define (and-map f l) 
   (for/and ([i l]) (if (f i) i #false))
 )
+
+;[X -> Boolean] [List-of X] -> [X or #false]
+;produces the first value who pass the condition, otherwise #false
+(define (or-map f l) 
+  (for/or ([i l]) (if (f i) i #false))
+)
