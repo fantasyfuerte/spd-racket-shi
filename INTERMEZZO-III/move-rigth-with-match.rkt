52a22f27cb623f4dfe562a6d626be71ea95aec48
leo <leandrofuerte75@gmail.com>
2026-08-14 11:28:21 -0700

    move-right function using match

---
+++ INTERMEZZO-III/move-rigth-with-match.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname move-rigth-with-match) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(require 2htdp/abstraction)
+
+;[List-of Posn] -> [List-of Posn]
+;moves each object right by delta-x pixels
+(define input `(,(make-posn 1 1) ,(make-posn 10 14)))
+(define output `(,(make-posn 4 1) ,(make-posn 13 14)))
+(check-expect (move-right input 3) output)
+(define (move-right l delta-x)
+  (for/list ([i l]) 
+    (match i
+      [(posn x y) (make-posn (+ x delta-x) y)]
+    )
+  )
+)
