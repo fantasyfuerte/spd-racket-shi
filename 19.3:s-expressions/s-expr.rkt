c4ef161bd87c97365764b6c48afde3ff1b686f52
leo <leandrofuerte75@gmail.com>
2026-08-14 21:04:16 -0700

    s-expressions data definition

---
+++ 19.3:s-expressions/s-expr.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname s-expr) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;an S-expr is one of:
+;-- Atom
+;-- SL
+
+;an Atom is one of:
+;-- Number
+;-- String
+;-- Symbol
+
+;an SL is one of:
+;-- '()
+;-- (cons S-expr SL)
+
+
