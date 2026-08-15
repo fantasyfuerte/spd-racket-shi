7a4ca7a771657cd4dfff4856eb01f8706bed60c6
leo <leandrofuerte75@gmail.com>
2026-08-15 07:32:00 -0700

    count sy in s-expressions

--- 19.3:s-expressions/s-expr.rkt
+++ 19.3:s-expressions/s-expr.rkt
@@ -21,3 +21,38 @@
   (or (string? x)
       (number? x)
       (symbol? x)))
+
+;S-expr Symbol -> N
+;counts all occurrences of sy in sexp
+(check-expect (count 'world 'hello) 0)
+(check-expect (count '(world hello) 'hello) 1)
+(check-expect (count '(((world) hello) hello) 'hello) 2)
+(define (count sexp sy)
+  (cond
+    [(atom? sexp)(count-atom sexp sy)]
+    [else (count-sl sexp sy)]
+  )
+)
+
+;SL Symbol -> N
+;counts all occurrences of sy in sl
+(define (count-sl sl sy)
+  (cond
+    [(empty? sl) 0]
+    [else 
+    (+
+      (count (first sl) sy)
+      (count-sl (rest sl) sy)) 
+    ]
+  )
+)
+
+;Atom Symbol -> N
+;counts all occurrences of sy in at
+(define (count-atom at sy)
+  (cond
+    [(number? at) 0]
+    [(string? at) 0]
+    [(symbol? at) (if (symbol=? at sy) 1 0)]
+  )
+)
