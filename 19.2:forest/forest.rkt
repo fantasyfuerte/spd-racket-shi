9a85676dfe69832c49b3fcac961f7a40084222ea
leo <leandrofuerte75@gmail.com>
2026-08-14 20:25:16 -0700

    family forest data definition

---
+++ 19.2:forest/forest.rkt
@@ -0,0 +1,40 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname forest) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;an FF (short for FamilyForest) is one of:
+;--'()
+;--(cons FT FF)
+;interpretation: a family-forest represents several families
+;(say, a town) and their ancestor trees
+
+(define-struct child [father mother name date eyes])
+;interpretation: (make-child a b c d f) combines the father a,
+;the mother b, the name c, the birth date d and the color of eyes f
+
+(define-struct no-parent [])
+(define NP (make-no-parent))
+
+;a FT (short for family tree) is one of:
+;-- NP
+;-- (make-child FT FT String N String)
+
+;family trees
+;older
+(define Bettina (make-child NP NP "Bettina" 1926 "green"))
+(define Carl (make-child NP NP "Carl" 1926 "green"))
+
+;middle
+(define Adam (make-child Carl Bettina "Adam" 1950 "hazel"))
+(define Dave (make-child Carl Bettina "Dave" 1955 "black"))
+(define Eva (make-child Carl Bettina "eva" 1965 "blue"))
+(define Fred (make-child NP NP "Fred" 1966 "pink"))
+
+;younger
+(define Gustav (make-child Fred Eva "Gustav" 1988 "brown"))
+
+
+;family forest 
+(define ff1 (list Carl Bettina))
+(define ff2 (list Fred Eva))
+(define ff3 (list Fred Eva Carl))
