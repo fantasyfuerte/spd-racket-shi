c7cb6209d20cddd301b923ba55f4d5cb8416d938
leo <leandrofuerte75@gmail.com>
2026-08-15 19:05:57 -0700

    substitute to simplify

---
+++ 19.6:simplifying-functions/subst-to-simplify.rkt
@@ -0,0 +1,36 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname subst-to-simplify) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;Any->Boolean
+;yields true if x is an atom
+(define (atom? x)
+  (or (string? x)
+      (number? x)
+      (symbol? x)))
+
+
+;S-expr Symbol Atom -> S-expr
+;replaces all occurrences of old in sexp with new
+(check-expect 
+  (substitute '(((world) bye ) bye) 'bye '42)
+  '(((world) 42) 42))
+(define (substitute sexp old new)
+  (local (;S-expr->S-expr
+          (define (for-sexp sexp)
+            (cond
+              [(atom? sexp) (for-atom sexp)]
+              [else (for-sl sexp)]))
+          ;SL -> S-expr
+          (define (for-sl sl)
+            (cond
+              [(empty? sl) '()]
+              [else (cons (for-sexp (first sl))
+                          (for-sl (rest sl)))]))
+          ;Atom -> S-expr
+          (define (for-atom at)
+            (cond
+              [(number? at) at]
+              [(string? at) at]
+              [(symbol? at) (if (equal? at old) new at)])))
+    (for-sexp sexp)))
