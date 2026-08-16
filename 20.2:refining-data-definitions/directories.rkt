10c44f79c036f7fe740236fa20d16edf2501cd8c
leo <leandrofuerte75@gmail.com>
2026-08-16 07:48:46 -0700

    data definition dir.v3 and file.v3 and representing figure 123 ex 335

--- 20.2:refining-data-definitions/directories.rkt
+++ 20.2:refining-data-definitions/directories.rkt
@@ -69,3 +69,31 @@
     [else (add1 (count-lofd (rest lofd)))]
   )
 )
+
+(define-struct file [name size content])
+;a File.v3 is a structure:
+;  (make-file String N String
+
+(define-struct dir.v3 [name dir files])
+;a Dir.v3 is a structure:
+;(make-dir.v3 String Dir* File*)
+
+;a Dir* is one of:
+;-- '()
+;-- (cons Dir.v3 Dir*)
+
+;a File.* is one of:
+;-- '()
+;-- (cons File.v3 File*)
+
+(define m3dir (make-dir.v3 "TS" 
+  (list 
+    (make-dir.v3 "Text" '() (list 
+      (make-file "part1" 99 "")
+      (make-file "part2" 52 "")
+      (make-file "part3" 17 ""))) 
+    (make-dir.v3 "Libs" (list 
+      (make-dir.v3 "Code" '() 
+        (list (make-file "hang" 8 "") (make-file "draw" 2 "")))
+      (make-dir.v3 "Docs" '() (list (make-file "read" 19 "")))) '()))
+  (list (make-file "read" 10 ""))))
