b1e51ae508102167c56e71c052fcd2d7f145d09d
leo <leandrofuerte75@gmail.com>
2026-08-23 21:21:54 -0700

    xepr data definition and exercise 363 and 364

---
+++ 22.1:xml-as-s-expressions/representing-xml.rkt
@@ -0,0 +1,23 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname representing-xml) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;an Xexpr.v0 (short for X-expression) is a one-item list:
+;  (cons Symbol '())
+
+;an Xexpr.v1 is a list:
+;  (cons Symbol [List-of Xexpr.v1])
+
+;an Xexpr.v2 is a list:
+;-- (cons Symbol Body)
+
+;a Body is one of:
+;-- '()
+;-- (cons Xexpr.v2 Body)
+;-- (cons [List-of Attribute] (cons Body))
+
+;an Attribute is a list of two items:
+;  (cons Symbol (cons String '()))
+
+(define ex1 '(transition ((from "seen-e") (to "seen-f"))))
+(define ex2 '(ul (li (word) (word))(li (word))))
