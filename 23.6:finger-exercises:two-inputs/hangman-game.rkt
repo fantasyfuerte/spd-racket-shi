c8e4b622b170bfa26fdf7b51f915269b6fe72bc9
leo <leandrofuerte75@gmail.com>
2026-08-29 10:27:26 -0700

    compare-word function exercise 396

--- 23.6:finger-exercises:two-inputs/hangman-game.rkt
+++ 23.6:finger-exercises:two-inputs/hangman-game.rkt
@@ -2,8 +2,13 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname hangman-game) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
+(require 2htdp/image)
+(require 2htdp/universe)
+
 ;an HM-Word is a [List-of Letter or "_"]
 ;interpretation "_" representsa letter to be guessed
+
+(define LETTERS (explode "abcdefghijklmnñopqrstuvwxyz"))
 
 ;HM-Word N -> String
 ;runs a simplistic hangman game, produces the current state
@@ -26,3 +31,25 @@
 ;HM-Word -> Image
 (define (render-word w)
   (text (implode w) 22 "black"))
+
+;HM-Word HM-Word KeyEvent -> HM-Word
+;produces guess with all the "_" where k revealed a letter
+(check-expect 
+  (compare-word '("d" "o" "g") '("_" "_" "g") "o") '("_" "o" "g"))
+(check-expect 
+  (compare-word '("d" "o" "g") '("_" "_" "g") "t") '("_" "_" "g"))
+(define (compare-word the-word guess k)
+  (cond
+    [(not(member? "_" guess)) guess]  
+    [else 
+      (if (and (blank? (first guess)) (equal? (first the-word) k))
+          (cons (first the-word) 
+                (compare-word (rest the-word) (rest guess) k))
+          (cons (first guess)
+                (compare-word (rest the-word) (rest guess) k)))]))
+
+;String -> Boolean
+;yields true if s is "_"
+(define (blank? x) (string=? "_" x))
+
+(define (do-nothing s ) s)
