f61fdc8786beb281d747f82f09e549dda53b68a5
leo <leandrofuerte75@gmail.com>
2026-08-29 07:44:46 -0700

    drop-function exercise 395

---
+++ 23.6:finger-exercises:two-inputs/drop-function.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname drop-function) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;[List-of Number] N -> [List-of Items]
+;produces the first n items on the list
+(check-expect (take '(1 2 3 4 5) 3) '(4 5))
+(check-expect (take '(1 2) 10) '())
+(define (take l n)
+  (cond
+    [(empty? l) '()]
+    [(= n 0) l]
+    [else (take (rest l) (sub1 n))]))
