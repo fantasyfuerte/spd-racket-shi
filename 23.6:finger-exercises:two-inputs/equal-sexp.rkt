c4b86e55b8cfeb2623daa3c9cb595ec1e7aa59d5
leo <leandrofuerte75@gmail.com>
2026-08-30 12:13:21 -0700

    s-expression data definition

---
+++ 23.6:finger-exercises:two-inputs/equal-sexp.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname equal-sexp) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;an S-expr (S-expression) is one of:
+;-- Atom
+;-- [List-of S-expr]
+
+;an Atom is one of:
+;-- Number
+;-- String
+;-- Symbol
