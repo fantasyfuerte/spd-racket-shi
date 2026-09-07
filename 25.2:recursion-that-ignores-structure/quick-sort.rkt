d91859edf74adb1b3128c07fdfd0ed105d01c58d
leo <leandrofuerte75@gmail.com>
2026-09-07 10:11:51 -0700

    quick-sort :)

---
+++ 25.2:recursion-that-ignores-structure/quick-sort.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname quick-sort) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;[List-of Number]-> [List-of Number]
+;produces a sorted version of l
+(check-expect (quick-sort< '(1 2 3 4 5)) '(1 2 3 4 5))
+(check-expect (quick-sort< '(5 3 2 4 1)) '(1 2 3 4 5))
+(define (quick-sort< l)
+  (cond
+    [(empty? l) '()]
+    [else (append (quick-sort< 
+                    (filter (lambda (x) (< x (first l))) (rest l)))
+                  (cons (first l)
+                  (quick-sort< 
+                    (filter (lambda (x) (>= x (first l))) (rest l)))))]))
