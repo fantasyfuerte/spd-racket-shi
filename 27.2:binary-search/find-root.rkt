23270cf45c6fec2ead22b526728e4a9346183f57
leo <leandrofuerte75@gmail.com>
2026-09-08 16:13:51 -0700

    fix test ex 445 and complete function find-root

--- 27.2:binary-search/find-root.rkt
+++ 27.2:binary-search/find-root.rkt
@@ -2,15 +2,29 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname find-root) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
+(define EPSILON 0.001)
+
 ;[Number -> Number] Number Number -> Numer
 ;determines R such that f has a root in [R ,(+ R EPSILON)]
 ;assume: f is continuous
 ;(2) (or (<- (f left) 0 (f right)) (<= (f right) 0 (f left)))
 ;generative: divides interval in half, the root is in one of the 
 ;halves, picks according to (2)
-(check-satisfied (find-root poly 3 5) zero?)
+(check-satisfied 
+  (find-root poly 3 6) 
+  (lambda (n) 
+    (zero? (poly(round n)))))
 (define (find-root f left right)
-  0)
+  (cond
+    [(<= (- right left) EPSILON) left]
+    [else
+      (local ((define mid (/ (+ left right) 2))
+              (define f@m (f mid)))
+              (cond
+                [(or (<= (f left) 0 f@m) (<= f@m 0 (f left)))
+                 (find-root f left mid)]
+                [(or (<= f@m 0 (f right)) (<= (f right) 0 f@m))
+                 (find-root f mid right)]))]))
 
 ;Number -> Number
 (define (poly x)
