9cba2b4effeb8034ad39fc0c367b905777da5419
leo <leandrofuerte75@gmail.com>
2026-09-08 09:25:20 -0700

    getlength ex437

---
+++ 26.3:structural-versus-generative-recursion/special.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname special) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(check-expect (getlength '(1 2 3 4)) 4)
+(check-expect (getlength '(1 4)) 2)
+(check-expect (getlength '()) 0)
+(define (getlength P)
+  (cond
+    [(empty? P) 0]
+    [else 
+      (add1
+        (getlength (rest P)))]))
+
+
+
