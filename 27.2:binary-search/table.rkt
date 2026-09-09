62f63e70786686a2c11fe23f182204780b56e064
leo <leandrofuerte75@gmail.com>
2026-09-09 09:36:22 -0700

    find-linear function ex 451

--- 27.2:binary-search/table.rkt
+++ 27.2:binary-search/table.rkt
@@ -2,7 +2,7 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname table) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
-(define-struct table [legth array])
+(define-struct table [length array])
 ;a Table is a structure:
 ;  (make-table N [N -> Number])
 
@@ -14,3 +14,28 @@
       (error "table2 is not defined for i != 0")))
 
 (define table2 (make-table 1 a2))
+
+(define l1 '(-2 -1 0 1 2))
+(define table3 (make-table 5 (lambda (x) (list-ref l1 x))))
+
+;Table N -> Number
+;looks up the ith value in array of t
+(define (table-ref t i)
+  ((table-array t) i))
+
+;Table -> Number
+;finds the smalles index for a root of the table
+;constraint: t is a monotonically increasing table
+(check-expect (find-linear table1) 0)
+(check-error (find-linear table2) "limit reached")
+(check-expect (find-linear table3) 2)
+(define (find-linear t)
+  (local(
+         (define length (table-length t))
+         (define (maptable i)
+           (if (= i length)
+               (error "limit reached")
+               (if (zero? (table-ref t i)) 
+                   i
+                   (maptable (add1 i))))))
+    (maptable 0)))
