0d911208ed31ae7f9794335ee34d361e58ce90c3
leo <leandrofuerte75@gmail.com>
2026-09-09 17:29:49 -0700

    file->list-of-lines function also completing ex 452

--- 27.3:a-glimpse-at-parsing/files.rkt
+++ 27.3:a-glimpse-at-parsing/files.rkt
@@ -2,6 +2,8 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname files) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
+(define NEWLINE "\n")
+
 ;a File is one of:
 ;-- '()
 ;-- (cons "\n" File)
@@ -10,3 +12,35 @@
 ;"\n" is the newline character
 
 ;a Line is a [List-of 1String]
+
+;File -> [List-of Line]
+;converts a file into a list of lines
+(check-expect (file->list-of-lines
+                (list "a" "b" "c" "\n"
+                      "d" "e" "\n"
+                      "f" "g" "h" "\n"))
+              (list (list "a" "b" "c")
+                    (list "d" "e")
+                    (list "f" "g" "h")))
+(define (file->list-of-lines afile) 
+  (cond
+    [(empty? afile) '()]
+    [else
+      (cons (first-line afile)
+            (file->list-of-lines (remove-first-line afile)))]))
+
+;File->Line
+;returns the first line of a file
+(define (first-line afile)
+  (cond
+    [(empty? afile) '()]
+    [(string=? (first afile) NEWLINE) '()]
+    [else (cons (first afile) (first-line (rest afile)))]))
+
+;File -> [List-of Lines]
+;removes the first line of a file
+(define (remove-first-line afile)
+  (cond
+    [(empty? afile) '()]
+    [(string=? (first afile) NEWLINE) (rest afile)]
+    [else (remove-first-line (rest afile))]))
