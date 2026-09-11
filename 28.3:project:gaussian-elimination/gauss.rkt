ece16a8373b0145e880541daaa7b235331e17e60
leo <leandrofuerte75@gmail.com>
2026-09-11 10:25:51 -0700

    systems of equations data representation

---
+++ 28.3:project:gaussian-elimination/gauss.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname gauss) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;a SOE (short for System of Equations) is a non-empty Matrix
+;constraint for (list r1 .. rn), (length ri) is (+ n 1)
+;interpretation: represents a system of linear equations
+
+;an Equation is a [List-of Number]
+;constraint: an Equation contains at least two numbers
+;interpretation: if (list a1 ... an b) is an Equation,
+;then a1, ..., an are the left-hand-side variable coefficients
+;and b is the right-hand side
+
+;a Solution is a [List-of Number]
+(define M; an SOE
+  (list (list 2 2 3 10); an Equation
+        (list 2 5 12 31)
+        (list 4 1 -2 1)))
+
+(define S '(1 1 2)) ; Solution
