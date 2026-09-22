ea42bcea354c8e14da4a64554c0f3bc01adc3bec
leo <leandrofuerte75@gmail.com>
2026-09-22 09:08:16 -0700

    factorial v1 function

---
+++ 32.3:transforming-functions-into-accumulator-style/factorial.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname factorial) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;N -> N
+;computes (* n (- n 1) (- n 2) ... 1)
+(check-expect (!.v1 3) 6)
+(define (!.v1 n)
+  (cond
+    [(zero? n) 1]
+    [else (* n (!.v1 (sub1 n)))]))
