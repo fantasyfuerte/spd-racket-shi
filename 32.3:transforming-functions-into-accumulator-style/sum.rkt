b026454b3327c054988599f0379d79d049addcd6
leo <leandrofuerte75@gmail.com>
2026-09-22 08:51:54 -0700

    definition of sum.v1, template of sum.v2 and signature and purpose statements of both

---
+++ 32.3:transforming-functions-into-accumulator-style/sum.rkt
@@ -0,0 +1,23 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname sum) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;[List-of Number] -> Number
+;produces the sum of the numbers on l
+(define (sum.v1 l)
+  (cond
+    [(empty? l) 0]
+    [else (+ (first l) (sum.v1 (rest l)))]))
+
+;[List-of Number] -> Number
+;produces the sum of the numbers on l
+(define (sum.v2 l)
+  (local (;[List-of Number] ??? -> Number
+          ;computes the sum of the numbers on l
+          ;accumulator ...
+          (define (sum/a l a)
+            (cond
+              [(empty? l) ...]
+              [else (... (sum/a (rest l)
+                                ... a ...)...)])))
+    (sum/a l ...)
