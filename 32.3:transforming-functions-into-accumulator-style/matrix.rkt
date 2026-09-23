a3b38b930ffe0c961f6e14d05859b2a7e58fd7c8
leo <leandrofuerte75@gmail.com>
2026-09-23 10:13:52 -0700

    rotate definition

---
+++ 32.3:transforming-functions-into-accumulator-style/matrix.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname matrix) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;Matrix -> Matrix
+;finds a row that doesn't start with 0 and
+;uses it as the first one
+;generative: moves the first row to last place
+;no termination: if all rows start with 0
+(check-expect (rotate '((0 4 5) (1 2 3)))
+              '((1 2 3) (0 4 5)))
+(define (rotate M)
+  (cond 
+    [(not (= (first (first M)) 0)) M]
+    [else 
+      (rotate (append (rest M) (list (first M))))]))
