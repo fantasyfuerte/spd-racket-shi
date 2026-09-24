8f7a95c51e879e8274d137fec9c71eecdc209a34
leo <leandrofuerte75@gmail.com>
2026-09-23 19:01:59 -0700

    split-structural definition ex 508

--- 32.4:a-graphical-editor-with-mouse/editor-with-mouse.rkt
+++ 32.4:a-graphical-editor-with-mouse/editor-with-mouse.rkt
@@ -2,6 +2,8 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname editor-with-mouse) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
+(require 2htdp/image)
+
 (define FONT-SIZE 11)
 (define FONT-COLOR "black")
 
@@ -17,4 +19,14 @@
 ;an interactive editor, (reverse p) corresponds to
 ;the text to the left of the cursor and s to the text on the right
 
-
+;[List-of 1String] -> N 
+;produces an editor with the cursor at the mouse x-coordinate
+(define (split-structural ed x)
+  (local (
+          (define plain (implode ed))
+          (define selected (round (/ x FONT-SIZE)))
+          (define pre 
+            (reverse (explode (substring plain 0 selected))))
+          (define post 
+            (explode (substring plain selected (string-length plain)))))
+    (make-editor pre post)))
