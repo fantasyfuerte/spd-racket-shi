ef9556f07a601891e8b11053313877f23ce02dad
leo <leandrofuerte75@gmail.com>
2026-09-25 11:46:53 -0700

    lam data definition and examples

---
+++ 33.1:accumulators-and-trees/compiler.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname compiler) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;a Lam is one of:
+;-- a Symbol
+;-- (list 'λ (list Symbol) Lam)
+;-- (list Lam Lam)
+
+;examples
+(define ex1 '(λ (x) x))
+(define ex2 '(λ (x) y))
+(define ex3 '(λ (y) (λ (x) y)))
+(define ex4 '((λ(x) (x x)) (λ (x) (x x))))
